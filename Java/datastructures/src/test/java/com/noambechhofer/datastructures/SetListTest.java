358ffedb48315a05d395b9262003c731e7144316
NoamBechhofer <noam.bechhofer@gmail.com>
2023-08-12 23:39:52 -0400

    organizded imports

--- Java/datastructures/src/test/java/com/noambechhofer/datastructures/SetListTest.java
+++ Java/datastructures/src/test/java/com/noambechhofer/datastructures/SetListTest.java
@@ -1,11 +1,14 @@
 package com.noambechhofer.datastructures;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 // import com.noambechhofer.datastructures.DuplicateElementException;
